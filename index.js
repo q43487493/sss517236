ba4d96ab4735cd8930ecd957044dbe10d91bdb90
chen janmin <q43487493@yahoo.com.tw>
2018-01-27 01:42:25 +0800

    'ok'

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var bot = linebot({
 });
 
 
-//google與試算表權杖
+//Google的憑證及權杖
 var myClientSecret={"installed":{"client_id":"479898043718-lup8n5jqu4966evfttbaqi54u8g0rk4c.apps.googleusercontent.com","project_id":"praxis-granite-191610","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"Xw2vL6zIcBNxtKr8o381KQWo","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
 var auth = new googleAuth();
 var oauth2Client = new auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
@@ -198,7 +198,7 @@ function processText(myMsg){
 	  
 	else if (myMsg === '1234'){
 	    if (!deviceIsConnected()){
-         returnResult='裝置未連接，無法啟用!';
+         myResult='裝置未連接，無法啟用!';
 		} 
        else{
 		 admin = 1234 ;
