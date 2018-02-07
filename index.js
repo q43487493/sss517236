0d39169a38354db7d65f539379d9c7bd8629f1aa
chen janmin <q43487493@yahoo.com.tw>
2018-02-07 22:15:37 +0800

    'ok'

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ var bot = linebot({
 });
 
 //google與試算表權杖
-var myClientSecret={"installed":{"client_id":"479898043718-lup8n5jqu4966evfttbaqi54u8g0rk4c.apps.googleusercontent.com","project_id":"praxis-granite-191610","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"Xw2vL6zIcBNxtKr8o381KQWo","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+var clientsecret={"installed":{"client_id":"479898043718-lup8n5jqu4966evfttbaqi54u8g0rk4c.apps.googleusercontent.com","project_id":"praxis-granite-191610","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"Xw2vL6zIcBNxtKr8o381KQWo","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
 var auth = new googleAuth();
-var oauth2Client = new auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
+var oauth2Client = new auth.OAuth2(clientsecret.installed.client_id,clientsecret.installed.client_secret, clientsecret.installed.redirect_uris[0]);
 oauth2Client.credentials ={"access_token":"ya29.Gls9BQU9XIxCcwKmh8x1DNBFw7KtZUtnz5yWIzzdNkGlLIuVSHwavOwF1brXAauVGCY5CLB6_bI_hi6ceK7vGrLuHsUxU5AHjMCUcNS6U42xvMxMmHHOct6nV23A","refresh_token":"1/ClJ-30WGZ8vjXlkHGKxkDw6yzVnowbf2pseYf2iMrk8","token_type":"Bearer","expiry_date":1515497815494}
 
-var mySheetId='1knco-UIs-D8iX10zBba9sO0q0c-2uv5RdLIeFK-tBD0';//試算表id
-var myBoardVars={device: '8QwwV'}; //Webduino的device id
-var myBoardVars2={ device: '10Q28gDy', transport: 'mqtt'};
-var myBoard;  
-var myBoard2;
+var SheetId='1knco-UIs-D8iX10zBba9sO0q0c-2uv5RdLIeFK-tBD0';//試算表id
+var device_id_1={device: '8QwwV'}; //Webduino的device id
+var device_id_2={device: '10Q28gDy', transport: 'mqtt'};
+var Board_1;  
+var Board_2;
 var admin = 0 ;       //管理員
 var people =0 ;      //家庭總人數
 var line_id = [] ;  // LINE 身分列表 
@@ -31,9 +31,9 @@ var user_id =[] ; //門禁卡身分列表
 var door = [] ;  //進or出門列表  
 var card_add = '' ;        //新增卡號
 var line_add = '' ;       //新增LINE使用者
-var do_1_2_3_4_5_6 = '' ;//暫存新增刪除總變數
+var admin_1_2_3_4_5_6 = '' ;//暫存新增刪除總變數
 var line_id_t = '' ;    //暫存line id 
-var user_id_t = '' ;   //暫存身分
+var user_id_t = '' ;   //暫存身分位置
 getdata(); 
 
 //LineBot處理文字訊息
@@ -58,12 +58,12 @@ bot.on('postback', function (event) {
   });
 });
 //處理webduino開發版
-boardReady(myBoardVars, true, function (board) {
-  myBoard=board;
+boardReady(device_id_1, true, function (board) {
+  Board_1=board;
   board.systemReset();
   board.samplingInterval = 250;
-  relay = getRelay(board, 7);
-  relay.off();
+  relay_1 = getRelay(board, 7);
+  relay_1.off();
   buzzer = getBuzzer(board, 2);  
   rfid = getRFID(board);
   rfid.read();     
@@ -72,12 +72,12 @@ boardReady(myBoardVars, true, function (board) {
     door_RFID(_uid);
   });  
 });   
-boardReady(myBoardVars2, true, function (board) {
-  myBoard2=board;
+boardReady(device_id_2, true, function (board) {
+  Board_2=board;
   board.systemReset();
   board.samplingInterval = 50;
-  relay2 = getRelay(board, 5);
-  relay2.off();
+  relay_2 = getRelay(board, 5);
+  relay_2.off();
  /* g3 = getG3(board, 2,3);
  g3.read(function(evt){
  bot.push('U79964e56665caa1f44bb589160964c84',  '目前pm25:'  + g3.pm25 ); 
@@ -98,7 +98,7 @@ function getdata() {
   var sheets = google.sheets('v4');
   sheets.spreadsheets.values.get({
     auth: oauth2Client,
-    spreadsheetId: mySheetId,
+    spreadsheetId: SheetId,
     range:encodeURI('資料庫'),  //試算表-工作表名稱
   }, function(err, response) {  
     var data = response.values;   // 讀取資料以二維陣列表示  [列][攔]
@@ -118,10 +118,10 @@ function getdata() {
   });
 } 
 //上傳試算表-資料庫
-function appendMyRow() {
+function add_date() {
   var request = {
     auth: oauth2Client,
-    spreadsheetId: mySheetId,
+    spreadsheetId: SheetId,
     range:encodeURI('資料庫'),
     insertDataOption: 'INSERT_ROWS',
     valueInputOption: 'RAW',
@@ -146,15 +146,15 @@ function appendMyRow() {
   });
 } 
 //處理line訊息
-function botText(myMsg){
-  var myResult='';
-  if (admin === 1234 && line_id_t === 'U79964e56665caa1f44bb589160964c84'){ myResult = botdoor(myMsg) }
-  else { myResult = setIoT(myMsg) } 
-  if (myResult!=''){}
-  else if (myMsg === '1234'){                                  
+function botText(message){
+  var Result='';
+  if (admin === 1234 && line_id_t === 'U79964e56665caa1f44bb589160964c84'){ Result = admin_door(message) }
+  else { Result = webduino(message) } 
+  if (Result!=''){}
+  else if (message === '1234'){                                  
     if (line_id_t === 'U79964e56665caa1f44bb589160964c84' ){   
       admin = 1234 ;                                           
-      myResult= [{ type: 'template',
+      Result= [{ type: 'template',
                   altText: 'this is a carousel template',
                   template: {
                     type: 'carousel', //選單旋轉
@@ -199,21 +199,21 @@ function botText(myMsg){
                 },{ type: 'text', text: "管理功能使用時間為90秒。"}]
       setTimeout(function () { 
         admin = 0 ;
-        do_1_2_3_4_5_6 = '' ;        
+        admin_1_2_3_4_5_6= '' ;        
         line_add = '';
         bot.push('U79964e56665caa1f44bb589160964c84', '管理權限啟動時間結束!'); 
       } , 1000 * 90);
     } 
     else{
-      myResult='您未具備管理身分，無法啟用!';  
+      Result='您未具備管理身分，無法啟用!';  
     } 
   }
-  else if (line_add === '新增' && myMsg === '159'){
+  else if (line_add === '新增' && message === '159'){
     var f = user_id.length;
     for (var k = 0 ; k<= f-2 ; k++){
       if (line_id[k] === line_id_t){
         bot.push('U79964e56665caa1f44bb589160964c84', '新增失敗，該用戶以代表一位使用者');
-        myResult = '您身分為:' + user_id[k] + '\n早就能用LINE來開門喔!'  ;
+        Result = '您身分為:' + user_id[k] + '\n早就能用LINE來開門喔!'  ;
         line_add = '';
         break;
       }
@@ -221,72 +221,72 @@ function botText(myMsg){
     if (line_add === '新增'){
       line_id[user_id_t] = line_id_t ;
       bot.push('U79964e56665caa1f44bb589160964c84', 'LINE UID新增成功!');
-      myResult = '已被新增\n可以使用LINE來開門囉!\n您身分為:' + user_id[user_id_t] ;
-      appendMyRow();
+      Result = '已被新增\n可以使用LINE來開門囉!\n您身分為:' + user_id[user_id_t] ;
+      add_date();
       line_add = '';
     }
   }   
-  else if (myMsg==='目前家中人數')	   
-    myResult='目前家中有' + people +'人'  ;   
+  else if (message==='目前家中人數')	   
+    Result='目前家中有' + people +'人'  ;   
   else{
-    myResult = '謝謝回覆!' ;
+    Result = '謝謝回覆!' ;
   } 
-  return myResult;
+  return Result;
 }
 //處理選單訊息
-function botpostback(myMsg){
-  var myResult = '';
-  if (myMsg === '新增使用者' && admin === 1234 || myMsg === '刪除使用者' && admin === 1234 || myMsg === '新增LINE UID' && admin === 1234 || myMsg === '刪除LINE UID' && admin === 1234 || myMsg === '新增卡號' && admin === 1234 || myMsg === '刪除卡號' && admin === 1234 ){
-    myResult = myMsg + '\n請輸入使用者名稱!';
-    if (myMsg === '新增使用者'){
-      do_1_2_3_4_5_6 = 1 ;
+function botpostback(message){
+  var Result = '';
+  if (message === '新增使用者' && admin === 1234 || message === '刪除使用者' && admin === 1234 || message === '新增LINE UID' && admin === 1234 || message === '刪除LINE UID' && admin === 1234 || message === '新增卡號' && admin === 1234 || message === '刪除卡號' && admin === 1234 ){
+    Result = message + '\n請輸入使用者名稱!';
+    if (message === '新增使用者'){
+      admin_1_2_3_4_5_6= 1 ;
     }
-    else if (myMsg === '刪除使用者'){
-      do_1_2_3_4_5_6 = 2 ;
+    else if (message === '刪除使用者'){
+      admin_1_2_3_4_5_6= 2 ;
     }
-    else if (myMsg === '新增LINE UID'){
-      do_1_2_3_4_5_6 = 3 ;
+    else if (message === '新增LINE UID'){
+      admin_1_2_3_4_5_6= 3 ;
     }
-    else if (myMsg === '刪除LINE UID'){
-      do_1_2_3_4_5_6 = 4 ;
+    else if (message === '刪除LINE UID'){
+      admin_1_2_3_4_5_6= 4 ;
     }
-    else if (myMsg === '新增卡號'){
-      do_1_2_3_4_5_6 = 5 ;
+    else if (message === '新增卡號'){
+      admin_1_2_3_4_5_6= 5 ;
     }
-    else if (myMsg === '刪除卡號'){
-      do_1_2_3_4_5_6 = 6 ;
+    else if (message === '刪除卡號'){
+      admin_1_2_3_4_5_6= 6 ;
     }
   }
-  return myResult;
+  return Result;
 } 
 //處理管理功能
-function botdoor(myMsg){
-  var myResult = '';
+function admin_door(message){
+  var Result = '';
   var f = (user_id.length);  
-  if (do_1_2_3_4_5_6 === 1  ){
+  if (admin_1_2_3_4_5_6=== 1  ){
     for (var j = 0 ; j <=f-2 ; j++){
-      if (user_id[j] === myMsg){
-        myResult = '此使用者已存在!';
-        myMsg ='' ;  
+      if (user_id[j] === message){
+        Result = '此使用者已存在!';
+        message ='' ;  
         break;
       }
     }
-    if (myMsg != ''){
+    if (message != ''){
       people = people + 1 ;
-      user_id.splice(0,0,myMsg);
+      user_id.splice(0,0,message);
       card_uid.splice(0,0,"");
       line_id.splice(0,0,"");
       door.splice(0,0,'在家中');
-      myResult = '"' + myMsg + '"新增成功!';
-      appendMyRow();
+      Result = '"' + message + '"新增成功!';
+      add_date();
     }
   }
-  else if (do_1_2_3_4_5_6 === 2 ){
+  else if (admin_1_2_3_4_5_6=== 2 ){
     for (var j = 0; j <= f-2; j++) {
-      if (user_id[j] === myMsg ){           
+      if (user_id[j] === message ){           
         if (f === 2){
-          myResult= '只剩"' + user_id[0]  +'"，無法刪除!';
-          myMsg ='';
+          Result= '只剩"' + user_id[0]  +'"，無法刪除!';
+          message ='';
           break;
         }
         else {
@@ -297,103 +297,103 @@ function botdoor(myMsg){
           card_uid.splice(j, 1);
           user_id.splice(j, 1);
           door.splice(j, 1);         
-          myResult= '"' + myMsg + '"刪除成功!';                                    
+          Result= '"' + message + '"刪除成功!';                                    
         }
-        myMsg ='';
-        appendMyRow();
+        message ='';
+        add_date();
         break;
       }   
     }
-    if (myMsg != ''){
-      myResult= '沒有這位使用者! \n請檢查是否輸入錯誤';              
+    if (message != ''){
+      Result= '沒有這位使用者! \n請檢查是否輸入錯誤';              
     }
   }    
-  else if (do_1_2_3_4_5_6 === 3){
+  else if (admin_1_2_3_4_5_6=== 3){
     for (var j = 0; j <= f-2; j++) {
-      if (user_id[j] === myMsg  ){
-        myResult = '請讓要新增的LINE使用者傳送"159"訊息!';
+      if (user_id[j] === message  ){
+        Result = '請讓要新增的LINE使用者傳送"159"訊息!';
         line_add = '新增' ;
         user_id_t = j ;
-        myMsg = '';
+        message = '';
         break;
       }           
     }
-    if (myMsg != ''){
-      myResult = '沒有這位使用者! \n請檢查是否輸入錯誤';    
+    if (message != ''){
+      Result = '沒有這位使用者! \n請檢查是否輸入錯誤';    
     }   
   }
-  else if (do_1_2_3_4_5_6 === 4){
+  else if (admin_1_2_3_4_5_6=== 4){
     for (var j = 0; j <= f-2; j++) {
-      if (user_id[j] === myMsg  ){
-        myResult = 'LINE UID刪除成功!';
+      if (user_id[j] === message  ){
+        Result = 'LINE UID刪除成功!';
         line_id[j] = '' ;
-        myMsg = '';
-        appendMyRow();
+        message = '';
+        add_date();
         break;
       }           
     }
-    if (myMsg != ''){
-      myResult = '沒有這位使用者! \n請檢查是否輸入錯誤';    
+    if (message != ''){
+      Result = '沒有這位使用者! \n請檢查是否輸入錯誤';    
     }
   }
-  else if (do_1_2_3_4_5_6 === 5){
+  else if (admin_1_2_3_4_5_6=== 5){
     for (var j = 0; j <= f-2; j++) {
-      if (user_id[j] === myMsg  ){
-        myResult = '請感應要新增的門禁卡';
+      if (user_id[j] === message  ){
+        Result = '請感應要新增的門禁卡';
         card_add = '新增' ;
         user_id_t = j ;
-        myMsg = '';
+        message = '';
         break;
       }           
     }
-    if (myMsg != ''){
-      myResult = '沒有這位使用者! \n請檢查是否輸入錯誤';    
+    if (message != ''){
+      Result = '沒有這位使用者! \n請檢查是否輸入錯誤';    
     }
   }
-  else if (do_1_2_3_4_5_6 === 6){
+  else if (admin_1_2_3_4_5_6=== 6){
     for (var j = 0; j <= f-2; j++) {
-      if (user_id[j] === myMsg  ){
-        myResult = '卡號刪除成功!';
+      if (user_id[j] === message  ){
+        Result = '卡號刪除成功!';
         card_uid[j] = '' ;
-        myMsg = '';
-        appendMyRow();
+        message = '';
+        add_date();
         break;
       }           
     }
-    if (myMsg != ''){
-      myResult = '沒有這位使用者! \n請檢查是否輸入錯誤';    
+    if (message != ''){
+      Result = '沒有這位使用者! \n請檢查是否輸入錯誤';    
     }
   }
-  do_1_2_3_4_5_6 = '' ;
-  return myResult ;
+  admin_1_2_3_4_5_6= '' ;
+  return Result ;
 }
 //處理webduino腳位
-function setIoT(fromMsg){
-  var returnResult='';  
-  if (fromMsg==='開門'){    
+function webduino(message){
+  var Result='';  
+  if (message==='開門'){    
     if (!deviceIsConnected())
-      returnResult='裝置未連接！';
+      Result='裝置未連接！';
     else{
-      returnResult = door_LINE(line_id_t);        
+      Result = door_LINE(line_id_t);        
     }     
   }
-  else if (fromMsg==='開燈'){    
+  else if (message==='開燈'){    
     if (!deviceIsConnected2())
-      returnResult='裝置未連接！';
+      Result='裝置未連接！';
     else{
-      returnResult='電燈已開啟!';
-      relay2.on();		 	            
+      Result='電燈已開啟!';
+      relay_2.on();		 	            
     }               			
   }
-  else if (fromMsg==='關燈'){    
+  else if (message==='關燈'){    
     if (!deviceIsConnected2())
-      returnResult='裝置未連接！';
+      Result='裝置未連接！';
     else{
-      returnResult='電燈已關閉!';
-      relay2.off();		 	            
+      Result='電燈已關閉!';
+      relay_2.off();		 	            
     }     
   }    
-  return returnResult;
+  return Result;
 }
 //使用RFID開門
 function door_RFID(UID){ 
@@ -411,7 +411,7 @@ function door_RFID(UID){
       bot.push('U79964e56665caa1f44bb589160964c84', '門禁卡新增成功!\n此卡代表身分為:' + user_id[user_id_t] );
       buzzer.play(buzzer_music([ {notes:"C7",tempos:"1"}]).notes ,buzzer_music([  {notes:"C7",tempos:"1"}]).tempos );
       card_add = '';	
-      appendMyRow();
+      add_date();
     }
   }		
   else{   
@@ -433,10 +433,10 @@ function door_RFID(UID){
             bot.push(line_id[t],'"' + user_id[j]  +'" 回家，家裡人數:' + people  + '人在家' );   
           }							
         }
-        appendMyRow(); 	
-        relay.on();
+        add_date(); 	
+        relay_1.on();
         setTimeout(function () {                   
-          relay.off();
+          relay_1.off();
         }, 1000 * 3);
         UID  = '' ;
         break;
@@ -468,10 +468,10 @@ function door_LINE(UID){
           bot.push(line_id[t],'"' + user_id[j]  +'" 回家，家裡人數:' + people  + '人在家' );  
         }                
       }
-      appendMyRow();   
-      relay.on();
+      add_date();   
+      relay_1.on();
       setTimeout(function () {                   
-        relay.off();
+        relay_1.off();
       }, 1000 * 3);
       UID  = '' ;
       break;
@@ -511,20 +511,20 @@ function buzzer_music(m) {
 }  
 //檢查webduino是否已連線成功
 function deviceIsConnected(){
-   if (myBoard===undefined)
+   if (Board_1===undefined)
       return false;
-   else if (myBoard.isConnected===undefined)
+   else if (Board_1.isConnected===undefined)
       return false;
    else
-      return myBoard.isConnected;
+      return Board_1.isConnected;
 }
 function deviceIsConnected2(){
-   if (myBoard2===undefined)
+   if (Board_2===undefined)
       return false;
-   else if (myBoard2.isConnected===undefined)
+   else if (Board_2.isConnected===undefined)
       return false;
    else
-      return myBoard2.isConnected;
+      return Board_2.isConnected;
 }
 
 
