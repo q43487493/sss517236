a566cb01ba148b78df11c486ca94fa952c900115
chen janmin <q43487493@yahoo.com.tw>
2018-02-21 17:39:27 +0800

    'ok'

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var pm_25 ;
 var humid ; 
 getdata(); 
 bot.push('U79964e56665caa1f44bb589160964c84', { type: 'image',originalContentUrl: 'https://i.imgur.com/zOCTs9N.png', previewImageUrl: 'https://i.imgur.com/zOCTs9N.png' });//主動回應圖片
-bot.push('U79964e56665caa1f44bb589160964c84', { type: 'image',originalContentUrl: 'https://imgur.com/bAZPahb', previewImageUrl: 'https://imgur.com/bAZPahb' });//主動回應圖片
+bot.push('U79964e56665caa1f44bb589160964c84', { type: 'image',originalContentUrl: 'https://i.imgur.com/bAZPahb.png', previewImageUrl: 'https://i.imgur.com/bAZPahb.png' });//主動回應圖片
 
 //LineBot處理文字訊息
 //bot.push('U79964e56665caa1f44bb589160964c84',[{ type: 'text', text: '目前浴室濕度高於75%，建議您開啟抽風機!'},Exhaust()]);
