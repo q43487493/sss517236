8894aaa1cfae043aa58572509be1692888780ceb
chen janmin <q43487493@yahoo.com.tw>
2018-02-27 02:07:39 +0800

    'ok'

--- index.js
+++ index.js
@@ -558,7 +558,8 @@ function webduino(message){
       Result='裝置未連接！';
     else{
       Result = door_LINE(line_id_t);        
-    }     
+    } 
+    Result = door_LINE(line_id_t);    
   }
   else if (message==='開燈'){    
     if (!deviceIsConnected2())
@@ -697,9 +698,9 @@ function door_LINE(UID){
         }                
       }
       add_date();   
-      relay_1.on();
+      //relay_1.on();
       setTimeout(function () {                   
-        relay_1.off();
+        //relay_1.off();
       }, 1000 * 3);
       UID  = '' ;
       break;
